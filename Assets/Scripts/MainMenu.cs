bbc7d9e0630a4f88f5e800bf15796e8d88d74f57
EduardoTn <49573663+EduardoTn@users.noreply.github.com>
2024-10-20 14:26:47 -0300

    lvl 1

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
